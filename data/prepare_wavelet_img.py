71c0e108887220df01045d2e014e88319f6fad36
flyingbucket <2311067@mail.nankai.edu.cn>
2025-07-02 23:30:59 +0800

    feature:allow 5 channels in prepare_wavelet_img.py

--- data/prepare_wavelet_img.py
+++ data/prepare_wavelet_img.py
@@ -16,16 +16,19 @@ def resize(img, size, resample):
     return img
 
 
-def wavelet_decompose(img: Image.Image):
+def wavelet_decompose(img: Image.Image,add_ori=False):
     """Apply 2D Haar wavelet transform to grayscale image."""
     arr = np.array(img).astype(np.float32) / 255.0
     coeffs2 = pywt.swt2(arr,'haar',level=1)
     LL, (LH, HL, HH) = coeffs2[0]
-    wave = np.stack([LL, LH, HL, HH], axis=-1)
+    if not add_ori:
+        wave = np.stack([LL, LH, HL, HH], axis=-1)
+    else:
+        wave = np.stack([arr,LL, LH, HL, HH], axis=-1)
     return wave.astype(np.float32)  # shape (H/2, W/2, 4)
 
 
-def prepare_and_save(file, sizes, out_path):
+def prepare_and_save(file, sizes, out_path,add_ori):
     key = file.stem.zfill(5)
     img = Image.open(file).convert('L')  # 单通道灰度图
 
@@ -34,15 +37,15 @@ def prepare_and_save(file, sizes, out_path):
     img_lr = resize(img, sizes[0], Image.BICUBIC)
     img_sr = resize(img_lr, sizes[1], Image.BICUBIC)
     # Wavelet
-    wave_target= wavelet_decompose(img_hr)
-    wave_in=wavelet_decompose(img_sr)
+    wave_target= wavelet_decompose(img_hr,add_ori=add_ori)
+    wave_in=wavelet_decompose(img_sr,add_ori=add_ori)
 
     # Save
     img_lr.save(f'{out_path}/lr_{sizes[0]}/{key}.png')
     np.save(f'{out_path}/sr_{sizes[0]}_{sizes[1]}/{key}.npy', wave_in,allow_pickle=False)
     np.save(f'{out_path}/hr_{sizes[1]}/{key}.npy', wave_target,allow_pickle=False)
 
-def main(img_dir, out_path, sizes):
+def main(img_dir, out_path, add_ori,sizes):
     img_dir = Path(img_dir)
     out_path = Path(out_path)
     files = sorted([p for p in img_dir.rglob('*') if p.suffix.lower() in ['.jpg', '.png']])
@@ -53,7 +56,7 @@ def main(img_dir, out_path, sizes):
     (out_path / f'sr_{sizes[0]}_{sizes[1]}').mkdir(parents=True, exist_ok=True)
 
     for file in tqdm(files, desc="Processing"):
-        prepare_and_save(file, sizes, out_path)
+        prepare_and_save(file, sizes, out_path,add_ori)
 
     print(f"\n✅ Finished saving {len(files)} images to {out_path}")
 
@@ -63,7 +66,8 @@ if __name__ == '__main__':
     parser.add_argument('--path', '-p', type=str, required=True, help='Input image folder')
     parser.add_argument('--out', '-o', type=str, required=True, help='Output base folder')
     parser.add_argument('--size', type=str, default='64,512', help='Low,High resolution sizes (e.g. 64,512)')
+    parser.add_argument('--add_ori',type=bool,required=True,help='Whether to add original image to wavelet decomposition')
     args = parser.parse_args()
 
     sizes = [int(x.strip()) for x in args.size.split(',')]
-    main(args.path, args.out, sizes)
+    main(args.path, args.out,args.add_ori, sizes)
